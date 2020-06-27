0cb0692bd154e3c6d46a0f7e5025537b85137ba3
Bhairavee <bhairavichitnis23@gmail.com>
2020-06-27 21:32:06 +0530

    sorting the nav links as per menu_order

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -10,17 +10,22 @@ const Navbar = () => {
                   edges {
                     node {
                       slug
+                      menu_order
                     }
                   }
                 }
             }
         `
     )
-    console.log(data);
+
+    const navData = data.allWordpressPage.edges.map(el => el.node)
+    navData.sort((a,b) => ((a.menu_order > b.menu_order) ? 1 : ((b.menu_order > a.menu_order) ? -1 : 0)));
+    console.log(navData);
+
     return (
         <ul className="flex space-x-24 items-center">
             <li className="mr-3 flex-1"><NavPill data="/"/></li>
-            {data.allWordpressPage.edges.map(({ node }) => (
+            {navData.map((node) => (
                 <li className="mr-3 flex-1"><NavPill data ={node.slug}/></li>
             ))}
         </ul>
